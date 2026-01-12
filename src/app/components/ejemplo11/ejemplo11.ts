237841a1ae6708fcb8c99e4361f7b770011737d9
diegoteis <diegoteis122@gmail.com>
2026-01-12 14:48:27 +0100

    ejemplo11

---
+++ src/app/components/ejemplo11/ejemplo11.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo11',
+  imports: [],
+  templateUrl: './ejemplo11.html',
+  styleUrl: './ejemplo11.css',
+})
+export class Ejemplo11 {
+  frase = ' aaaaaaaaaaaaaaa <b>angular</b> <script>alert("lol xd")</script>';
+}
