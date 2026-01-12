8f9b3cc83a0526c37b3d1725380ab87f2cd4d464
diegoteis <diegoteis122@gmail.com>
2026-01-12 13:44:27 +0100

    cambios mejores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
-import { Ejemplo0 } from './components/ejemplo0/ejemplo0';
-import { Ejemplo1 } from './components/ejemplo1/ejemplo1';
-import { Ejemplo2 } from './components/ejemplo2/ejemplo2';
-import { Ejemplo3 } from './components/ejemplo3/ejemplo3';
+import { Ejemplo0 } from './components/ejemplo00/ejemplo0';
+import { Ejemplo1 } from './components/ejemplo01/ejemplo1';
+import { Ejemplo2 } from './components/ejemplo02/ejemplo2';
+import { Ejemplo3 } from './components/ejemplo03/ejemplo3';
 import { Ejemplo3b } from './components/ejemplo3b/ejemplo3b';
 import { Ejemplo4b } from './components/ejemplo4b/ejemplo4b';
-import { Ejemplo4 } from './components/ejemplo4/ejemplo4';
-import { Ejemplo5 } from './components/ejemplo5/ejemplo5';
-import { Ejemplo6 } from './components/ejemplo6/ejemplo6';
-import { Ejemplo7 } from './components/ejemplo7/ejemplo7';
+import { Ejemplo4 } from './components/ejemplo04/ejemplo4';
+import { Ejemplo5 } from './components/ejemplo05/ejemplo5';
+import { Ejemplo6 } from './components/ejemplo06/ejemplo6';
+import { Ejemplo7 } from './components/ejemplo07/ejemplo7';
 
 export const routes: Routes = [
   { path: 'ejemplo0', component: Ejemplo0 },
--- src/app/components/ejemplo07/ejemplo7.ts
+++ src/app/components/ejemplo07/ejemplo7.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ejemplo7',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './ejemplo7.html',
-  styleUrl: './ejemplo7.css',
+  styleUrls: ['./ejemplo7.css'],
 })
 export class Ejemplo7 {
-
+  theme = 'pastel';
 }
