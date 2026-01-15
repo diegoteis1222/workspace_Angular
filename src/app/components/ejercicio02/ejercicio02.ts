095c895c869f2c5b6407411ae201a5df5909b9cf
diegoteis <diegoteis122@gmail.com>
2026-01-15 10:02:13 +0100

    ejercicio2

---
+++ src/app/components/ejercicio02/ejercicio02.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio02',
+  imports: [],
+  templateUrl: './ejercicio02.html',
+  styleUrl: './ejercicio02.css',
+})
+export class Ejercicio02 {
+
+}
