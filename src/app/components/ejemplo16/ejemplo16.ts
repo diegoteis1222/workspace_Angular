ebb3e8e9211d73419117990ea5a27ee134abe9e7
diegoteis <diegoteis122@gmail.com>
2026-01-15 09:46:52 +0100

    ejemplo16

---
+++ src/app/components/ejemplo16/ejemplo16.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-ejemplo16',
+  imports: [MatButtonModule, MatIconModule],
+  templateUrl: './ejemplo16.html',
+  styleUrl: './ejemplo16.css',
+})
+export class Ejemplo16 {
+
+}
