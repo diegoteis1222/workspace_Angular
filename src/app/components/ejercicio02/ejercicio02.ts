a6fcd573aaf69b2c8932f8248e24fae33cfb71bb
diegoteis <diegoteis122@gmail.com>
2026-01-16 09:50:29 +0100

    ejercicio2

--- src/app/components/ejercicio02/ejercicio02.ts
+++ src/app/components/ejercicio02/ejercicio02.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio02',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ejercicio02.html',
   styleUrl: './ejercicio02.css',
 })
 export class Ejercicio02 {
+  altura = 0;
+  peso = 0;
+  imc = 0;
 
+  handleSubmit() {
+    this.imc = this.peso / (this.altura * this.altura);
+  }
 }
