4ad9c1bbff31dcfd0d7c386e6f02dad584bc846d
diegoteis <diegoteis122@gmail.com>
2026-01-21 15:12:10 +0100

    ejemplo20c

---
+++ src/app/components/ejemplo20c/ejemplo20c.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { map, of } from 'rxjs';
+
+@Component({
+  selector: 'app-ejemplo20c',
+  imports: [CommonModule],
+  templateUrl: './ejemplo20c.html',
+  styleUrl: './ejemplo20c.css',
+})
+export class Ejemplo20c {
+  precioConIva$ = of(100).pipe(
+    map((precioSinIva) => precioSinIva * 1.21),
+    map((precioSinIva) => precioSinIva.toFixed(2)),
+    map((precioSinIva) => precioSinIva + '€')
+  );
+}
